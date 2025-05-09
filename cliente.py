99bce8df7a469528609cba31c2d4ceec1d792ebf
mtinta <68405669+mtinta@users.noreply.github.com>
2025-05-09 05:47:17 -0500

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -4,7 +4,7 @@ import uuid
 import threading
 import time
 from time import sleep
-
+import os
 app = Flask(__name__)
 
 class RpcClient:
@@ -79,4 +79,4 @@ def index():
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
